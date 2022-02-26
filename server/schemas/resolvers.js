460cb7baf330f1054fc680e40e99eebc61bdae90
Robert Sandoval <robsand1992@yahoo.com>
2022-02-26 10:16:02 -0800

    Added resolvers and modified server to accept Apollo server for GraphQL

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,66 @@
+const { AuthenticationError} = require('apollo-server-express')
+const { Book, User} = require('../models')
+const bookSchema = require('../models/Book')
+const { signToken } = require('../utils/auth')
+
+const resolvers = {
+    Query: {
+        user: async (parent, {username}) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('savedBooks')
+        }
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args)
+            const token = signToken(user)
+
+            return { token, user}
+        },
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email })
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials')
+            }
+
+            const correctPw = await user.isCorrectPassword(password)
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials')
+            }
+
+            const token = signToken(user)
+            return { token, user}
+        },
+
+        saveBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id},
+                    { $addToSet: { savedBooks: bookId}},
+                    { new: true}
+                ).populate('savedBooks')
+                return updatedUser
+            }
+            throw new AuthenticationError('You need to be logged in!')
+        },
+
+        deleteBook: async (parent, { bookId}, context) => {
+            if(context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    { $pull: { savedBooks: bookId}},
+                    { new: true}
+                ).populate('savedBooks')
+                return updatedUser
+            }
+            throw new AuthenticationError('You need to be logged in!')
+        }
+    }
+}
+
+module.exports = resolvers
