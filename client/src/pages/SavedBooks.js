d7544f3a5af3b0eb2b7e3e31c894002cf65cf21b
Robert Sandoval <robsand1992@yahoo.com>
2022-03-03 11:24:09 -0800

    refactored sign up form from restful API to graphQL mutations

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-import { useQuery } from '@apollo/client'
-import { deleteBook } from '../utils/API';
+
+import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
@@ -11,7 +11,30 @@ const SavedBooks = () => {
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
 
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const token = Auth.loggedIn() ? Auth.getToken() : null;
 
+        if (!token) {
+          return false;
+        }
+
+        const response = await getMe(token);
+
+        if (!response.ok) {
+          throw new Error('something went wrong!');
+        }
+
+        const user = await response.json();
+        setUserData(user);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getUserData();
+  }, [userDataLength]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -78,29 +101,3 @@ const SavedBooks = () => {
 };
 
 export default SavedBooks;
-
-
-/*   useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]); */
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -32,7 +32,9 @@ const SearchBooks = () => {
     }
 
     try {
+      
       const response = await searchGoogleBooks(searchInput);
+      
 
       if (!response.ok) {
         throw new Error('something went wrong!');
